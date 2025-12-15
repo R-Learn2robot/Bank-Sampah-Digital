f089d6fce77ce8031de7810d236fe5caf1505818
R-Learn2robot <radityawp2006@gmail.com>
2025-12-15 19:44:10 +0700

    rapih2 dan menambahkan fungsi edit

--- include/function.h
+++ include/function.h
@@ -6,25 +6,13 @@
 #include <stdlib.h>
 
 typedef struct {
-    char nama[100];
-    char nik[16];
-    char rt[2];
-    char rw[2];
-    char alamat[100];
+    char nama[101];
+    char nik[17];
+    char rt[3];
+    char rw[3];
+    char alamat[101];
     int poin;
 }Warga;
-
-#ifdef _WIN32
-    #define CLEAR "cls"
-#else
-    #define CLEAR "clear"
-#endif
-
-#define MAX_WARGA 50
-
-#define FILE_NAME "data/tes.txt"
-
-#define FILE_TRANSAKSI "transaksi.txt"//Faiq coba tambahakan ini
 typedef struct {
     char nik[20];
     char nama[100];
@@ -33,14 +21,45 @@ typedef struct {
     char tanggal[15]; 
 } Riwayat;
 
+#ifdef _WIN32
+    #define CLEAR "cls"
+#else
+    #define CLEAR "clear"
+#endif
+
+// variabel global
+extern Warga *data; // array of struct          
+extern int jlhWarga; // ada berapa warga yg terdaftar         
+extern int kapasitasWarga; // kapasitas data
+
+#define DATA_WARGA "data/dataWarga.txt"
+#define FILE_TRANSAKSI "data/transaksi.txt"//Faiq coba tambahakan ini
 
 
-void tambahWarga();
-void tampilData();
-void sortRWRTNama();
+// === DEKLARASI FUNGSI ===
+// fungsi lain-lain
 void save();
 void load();
-void pause();
-void listSemuaWarga();
+void pause(); 
+void cekKapasistas();
+void hapusMemori();
+
+// fungsi pencarian
+int cariIndexNIK(const char *nik);
+
+//fungsi sorting
+void sortRWRTNama();
+
+// fungsi tambah data warga
+void tambahWarga();
+
+// fungsi menampilkan data warga
+void tampilkanListWarga();
+void pencarianDataNIK();
+void liatDataWarga();
+
+// fungsi edit data
+void masukkanDataBaru(const int index);
+void editDataWarga();
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,20 +3,23 @@
 
 
 int main() {
-    int pilih;
+    int input;
     load();
     sortRWRTNama();
     save();
     do
     {
         system(CLEAR);
-        printf("===MENU===\n");
-        printf("1) tambah data\n");
-        printf("2) list data\n");
-        printf("3) exit\n");
-        scanf("%d", &pilih);
+        printf("=== MENU ===\n");
+        printf("1) Tambah data warga\n");
+        printf("2) Melihat data warga\n");
+        printf("3) Hapus data warga(WIP)\n");
+        printf("4) Edit data warga\n");
+        printf("0) exit\n");
+        printf("Masukkan input anda: ");
+        scanf("%d", &input);
         getchar();
-        switch (pilih)
+        switch (input)
         {
         case 1:
             tambahWarga();
@@ -26,14 +29,23 @@ int main() {
         case 2:
             sortRWRTNama();
             save();
-            tampilData();
+            liatDataWarga();
             pause();
             break;
-        case 3:
+        case 3: //belum ada fungsinya
+            break;
+        case 4:
+            editDataWarga();
+            pause();
+            break;
+        case 0:
+            save();
+            hapusMemori();
+            printf("\nProgram selesai");
             return 0;
-        
         default:
             printf("invalid");
+            pause();
             break;
         }
     } while (1);
